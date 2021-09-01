a1ccced397a1f6e395ae7cad62fb4ef7fb47d2a2
danielnlz01 <38385574+danielnlz01@users.noreply.github.com>
2021-08-31 20:42:54 -0500

    Add files via upload

---
+++ main(1).cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+using namespace std;
+
+//variables go here
+int num1;
+int num2;
+int res;
+
+int main()
+{
+  cout << "let's add 2 numbers" << endl;
+  
+  cout << "number 1: ";
+  cin >> num1;
+  
+  cout << "number 2: ";
+  cin >> num2;
+  
+  res=num1+num2;
+  cout << "the result of adding " << num1 << " + " << num2 << " is " << res << endl;
+  
+  return 0;
+}
+
+
